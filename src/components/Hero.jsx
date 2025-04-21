e9bb9359a6959664e57bc95811a76555596248dd
Franco Giacobbe <frandg47@gmail.com>
2025-04-21 17:53:18 -0300

    modificando estilos para mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,8 @@ const Hero = () => {
                 </div>
             </section>
             <div className="hero-presentation">
-                <h2>GIO Arquitectura</h2>
+                <h2>GIO</h2>
+                <h5>Construcción & Diseño</h5>
                 <p>somos un estudio joven que diseña con los pies en la tierra. Nos especializamos en crear proyectos funcionales, estéticos y, sobre todo, ajustados al bolsillo del cliente. Sabemos lo que cuesta construir, por eso cada diseño está pensado para ser posible, accesible y bien resuelto desde el primer boceto hasta el último detalle.</p>
             </div>
         </>
